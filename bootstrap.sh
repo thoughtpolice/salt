eb742371a5ec354fcfa41fc584d01283203e23f4
Austin Seipp <as@hacks.yi.org>
2011-10-08 23:38:02 -0500

    Add a much better bootstrap.sh script to install NaCl for you!

---
+++ bootstrap.sh
@@ -0,0 +1,87 @@
+#!/usr/bin/env sh
+
+DEV=1
+VERSION=nacl-20110221
+
+# No configuration needed past this point
+URL=http://hyperelliptic.org/nacl/$VERSION.tar.bz2
+CURL="curl -f#L"
+HSNACLDIR=$HOME/.hs-nacl
+TARBALL=$HSNACLDIR/$VERSION.tar.bz2
+
+say () {
+    echo "=== " $1 " ================"
+    return 0
+}
+
+if [ $DEV -eq 1 ]; then
+    CABAL=cabal-dev
+else
+    CABAL=cabal
+fi
+
+# initialize
+if [ ! -d "$HSNACLDIR" ]; then
+    mkdir $HSNACLDIR
+fi
+
+# download
+if [ ! -f "$TARBALL" ]; then
+    say "Downloading $VERSION.tar.bz2..."
+    $CURL $URL -o $TARBALL
+else
+    say "Tarball already downloaded."
+fi
+
+# extract
+if [ ! -d "$HSNACLDIR/$VERSION" ]; then
+    say "Extracting"
+    tar -jxf $TARBALL -C $HSNACLDIR
+else
+    say "Source code already extracted."
+fi
+
+# build!
+if [ ! -d "$HSNACLDIR/$VERSION/build" ]; then
+    say "Now building $VERSION"
+    say "This is going to take a while, grab some coffee..."
+    cd $HSNACLDIR/$VERSION/ && ./do
+    say "Done"
+else
+    say "Using already-completed build."
+fi
+
+SHORTHOST=`hostname | sed 's/\..*//' | tr -cd '[a-z][A-Z][0-9]'`
+ARCHGREP=`ghc --info | grep x86_64`
+if [ "$ARCHGREP" != "" ]; then
+    ARCH=amd64
+else
+    ARCH=x86
+fi
+
+say "ARCH = $ARCH, HOST = $SHORTHOST"
+
+INCDIR=$HSNACLDIR/$VERSION/build/$SHORTHOST/include/$ARCH/
+LIBDIR=$HSNACLDIR/$VERSION/build/$SHORTHOST/lib/$ARCH/
+
+# get test prerequisites
+say "Grabbing test prerequisuites..."
+#echo " cabal-dev install QuickCheck HUnit test-framework test-framework-quickcheck2 test-framework-hunit"
+$CABAL install QuickCheck HUnit test-framework test-framework-quickcheck2 test-framework-hunit
+
+# build with cabal 
+echo 
+say "Building with cabal..."
+#echo " cabal-dev install --extra-include-dirs=$INCDIR --extra-lib-dirs=$LIBDIR --enable-tests"
+$CABAL install --extra-include-dirs=$INCDIR --extra-lib-dirs=$LIBDIR --enable-tests
+
+# test
+echo 
+say "Testing..."
+$CABAL test
+echo
+say "Test results:"
+cat dist/test/NaCl-*-properties.log
+
+echo
+say "Completed"
--- test.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env sh
-cabal-dev clean && cabal-dev install $* --extra-include-dirs=/home/a/src/nacl-20110221/build/link/include/amd64 --extra-lib-dirs=/home/a/src/nacl-20110221/build/link/lib/amd64 --enable-tests && cabal-dev test && echo && cat dist/test/NaCl-*-properties.log
