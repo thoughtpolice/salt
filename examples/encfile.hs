dda81340fdd7712ac6c6fafb083dd076f7a74e7c
Austin Seipp <as@hacks.yi.org>
2011-10-10 09:44:33 -0500

    even better encfile example!

--- examples/encfile.hs
+++ examples/encfile.hs
@@ -8,7 +8,12 @@ import Data.ByteString.Char8 as S hiding (putStrLn)
 import Crypto.NaCl.Encrypt.Stream
 import Crypto.NaCl.Nonce
 
+import System.IO
 import System.FilePath
+
+import Data.Enumerator
+import Data.Enumerator.Binary
+import qualified Data.Enumerator.List as EL
 
 main :: IO ()
 main = putStrLn ("Key length is " ++ show keyLength) >> getArgs >>= go
@@ -17,19 +22,25 @@ main = putStrLn ("Key length is " ++ show keyLength) >> getArgs >>= go
           putStrLn "USAGE ./encfile [encrypt|decrypt <nonce>] <key> <infile>"
         go ("encrypt":key:file:[]) = do
           checkKey key
-          bs <- S.readFile file
-          n <- createRandomNonce nonceLength
-          let e = encryptXor n bs (pack key)
-          S.writeFile (file <.> "enc") e
+          -- create nonce and write it
+          n <- clearBytes 8 `liftM` createRandomNonce nonceLength
           S.writeFile (file <.> "nonce") (toBS n)
+          h <- openFile (file <.> "enc") WriteMode
+          run_ $ (enumFile file $= encEnee (pack key) n) $$ iterHandle h
+          hClose h
         go ("decrypt":nonce:key:file:[]) = do
           checkKey key
           n  <- fromBS `liftM` (S.readFile nonce)
           checkNonce n
-          bs <- S.readFile file
-          let e = decryptXor n bs (pack key)
-          S.writeFile (dropExtension file <.> "dec") e
+          h <- openFile (dropExtension file <.> "dec") WriteMode
+          run_ $ (enumFile file $= encEnee (pack key) n) $$ iterHandle h
+          hClose h
         go _ = error "Try --help"
+
+encEnee :: Monad m => SecretKey -> Nonce -> Enumeratee ByteString ByteString m b
+encEnee k = EL.mapAccum $ \n bs ->
+  let bs' = encryptXor n bs k
+  in (incNonce n, bs')
 
 checkKey :: String -> IO ()
 checkKey k = 
