e48b8112b68c0dfabab170609fb1310ad9b70619
Austin Seipp <as@hacks.yi.org>
2011-10-11 04:33:07 -0500

    Clean up benchmarks (now using random data,) and benchmark ALL the things!

--- bench/Bench.hs
+++ bench/Bench.hs
@@ -4,7 +4,10 @@ module Main
        ( main -- :: IO ()
        ) where
 import Data.Int
-import Criterion.Main
+import System.IO
+import System.FilePath
+
+import Criterion.Main hiding (run)
 import Data.ByteString as S
 
 import Crypto.NaCl.Hash as H
@@ -17,8 +20,6 @@ import Crypto.NaCl.Encrypt.Stream as Stream
 
 import Control.DeepSeq
 
-import Control.Monad.Trans
-
 import Data.Enumerator
 import Data.Enumerator.Binary
 import qualified Data.Enumerator.List as EL
@@ -29,7 +30,11 @@ instance NFData ByteString where
 main :: IO ()
 main = do
   s1 <- Sign.createKeypair
-  streamk1 <- randomBytes $ Stream.keyLength Nothing
+  streamk1 <- randomBytes $ Stream.keyLength $ Just AES128CTR
+  streamk2 <- randomBytes $ Stream.keyLength $ Just Salsa20
+  streamk3 <- randomBytes $ Stream.keyLength $ Just Salsa2012
+  streamk4 <- randomBytes $ Stream.keyLength $ Just Salsa208
+  streamk5 <- randomBytes $ Stream.keyLength $ Just XSalsa20
   
   defaultMain [ bgroup "Signing"
                 [ bench "createKeypair" $ nfIO Sign.createKeypair
@@ -39,10 +44,31 @@ main = do
                 , bench "verify 512" $ nf (signBench s1) $ pack [1..512]
                 ]
               , bgroup "Stream"
-                [ bench "pure streamgen 64/1000" $ nf (streamGenBench streamk1  64) 1000
-                , bench "enum encrypt 512mb /dev/null" $ nfIO $ streamEncBench streamk1 512
-                  --  This takes 45 minutes on my core i5.
---              , bench "enum encrypt 10gb /dev/null" $ nfIO $ streamEncBench streamk1 (1024*10)
+                [ bgroup "aes128ctr"
+                  [ bench "pure streamgen 4094/10000" $ nf (streamGenBench (Just AES128CTR) streamk1 4094) 10000
+                  , bench "enum1 encrypt 1gb random data" $ nfIO $ streamEncBench1 (Just AES128CTR) streamk1
+                  , bench "enum2 encrypt 1gb random data" $ nfIO $ streamEncBench2 (Just AES128CTR) streamk1                    
+                  ]
+                , bgroup "salsa20"
+                  [ bench "pure streamgen 4094/10000" $ nf (streamGenBench (Just Salsa20) streamk2 4094) 10000
+                  , bench "enum1 encrypt 1gb random data" $ nfIO $ streamEncBench1 (Just Salsa20) streamk2
+                  , bench "enum2 encrypt 1gb random data" $ nfIO $ streamEncBench2 (Just Salsa20) streamk2
+                  ]
+                , bgroup "salsa2012"
+                  [ bench "pure streamgen 4094/10000" $ nf (streamGenBench (Just Salsa2012) streamk3 4094) 10000
+                  , bench "enum1 encrypt 1gb random data" $ nfIO $ streamEncBench1 (Just Salsa2012) streamk3
+                  , bench "enum2 encrypt 1gb random data" $ nfIO $ streamEncBench2 (Just Salsa2012) streamk3
+                  ]
+                , bgroup "salsa208"
+                  [ bench "pure streamgen 4094/10000" $ nf (streamGenBench (Just Salsa208) streamk4 4094) 10000
+                  , bench "enum1 encrypt 1gb random data" $ nfIO $ streamEncBench1 (Just Salsa208) streamk4
+                  , bench "enum2 encrypt 1gb random data" $ nfIO $ streamEncBench2 (Just Salsa208) streamk4
+                  ]
+                , bgroup "default xsalsa20"
+                  [ bench "pure streamgen 4094/10000" $ nf (streamGenBench (Just XSalsa20) streamk5 4094) 10000
+                  , bench "enum1 encrypt 1gb random data" $ nfIO $ streamEncBench1 (Just XSalsa20) streamk5
+                  , bench "enum2 encrypt 1gb random data" $ nfIO $ streamEncBench2 (Just XSalsa20) streamk5
+                  ]
                 ]
               , bgroup "Hashing"
                 [ bgroup "sha512"
@@ -84,24 +110,32 @@ main = do
                 v  = Sign.verify pk sm
             in maybe False (== xs) v
 
-        streamGenBench :: Stream.SecretKey -> Int -> Int -> ByteString
-        streamGenBench sk sz i 
-          = go (createZeroNonce $ Stream.nonceLength Nothing) empty i
+        streamGenBench :: Maybe CryptoMode -> Stream.SecretKey -> Int -> Int -> ByteString
+        streamGenBench mode sk sz i 
+          = go (createZeroNonce $ Stream.nonceLength mode) empty i
           where go !_ !bs 0  = bs
-                go !n !_ !x = go (incNonce n) (Stream.streamGen Nothing n sz sk) (x-1)
+                go !n !_ !x = go (incNonce n) (Stream.streamGen mode n sz sk) (x-1)
 
-        streamEncBench :: Stream.SecretKey -> Int64 -> IO Int
-        streamEncBench sk mb = do
-          let total = mb*1024*1024 :: Int64
-              goI :: Int -> Int64 -> Nonce -> Iteratee ByteString IO Int
-              goI !x !t !n = do
-                v <- EL.head_
-                let !_ = Stream.encrypt Nothing n v sk
-                let l = fromIntegral $ S.length v
-                if (t+l) > total then
-                  return x
-                 else
-                  goI (x+1) (t+l) (incNonce n)
-          Data.Enumerator.run_
-                 (enumFile "/dev/zero"
-                  $$ (goI 0 0 $ createZeroNonce $ Stream.nonceLength Nothing))
+        streamEncBench1 :: Maybe CryptoMode -> Stream.SecretKey -> IO ByteString
+        streamEncBench1 mode sk = do
+          let nonce = createZeroNonce $ Stream.nonceLength mode
+              goI :: ByteString -> Nonce -> Iteratee ByteString IO ByteString
+              goI !_ !n = do
+                v <- EL.head
+                case v of
+                  Nothing -> return S.empty
+                  Just v_ -> goI (Stream.encrypt mode n v_ sk) (incNonce n)
+          run_ (enumFile "./testdata" $$ (goI S.empty nonce))
+
+        streamEncBench2 :: Maybe CryptoMode -> Stream.SecretKey -> IO ()
+        streamEncBench2 mode sk = do 
+            h <- openBinaryFile "/dev/null" WriteMode
+            let n = createZeroNonce $ Stream.nonceLength mode
+            run_ (encryptFileEnum "./testdata" mode sk n $$ iterHandle h)
+            hClose h
+            
+encryptFileEnum :: FilePath -> Maybe CryptoMode -> Stream.SecretKey -> Nonce -> Enumerator ByteString IO b
+encryptFileEnum f m sk n = enumFile f $= encryptEnee m sk n
+
+encryptEnee :: Monad m => Maybe CryptoMode -> Stream.SecretKey -> Nonce -> Enumeratee ByteString ByteString m b
+encryptEnee m k = EL.mapAccum $ \n bs -> (incNonce n, Stream.encrypt m n bs k)
