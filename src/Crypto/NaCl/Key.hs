92039754f99c4f63ade08469fb1a3819dcf631e3
Austin Seipp <mad.one@gmail.com>
2012-01-16 18:04:21 -0600

    Haddock boilerplate

--- src/Crypto/NaCl/Key.hs
+++ src/Crypto/NaCl/Key.hs
@@ -1,3 +1,16 @@
+-- |
+-- Module      : Crypto.NaCl.Sign
+-- Copyright   : (c) Austin Seipp 2011
+-- License     : MIT
+--
+-- Maintainer  : as@hacks.yi.org
+-- Stability   : experimental
+-- Portability : portable
+--
+-- This module exports two simple types, 'PublicKey' and 'SecretKey'
+-- which are merely @newtype@s for 'ByteString'. This is for type safety
+-- so you never confuse your keys or accidentally give them 'ByteString's
+-- when you shouldn't have.
 module Crypto.NaCl.Key
        ( PublicKey(..)
        , SecretKey(..)
